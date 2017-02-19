ba42ca477ccb089e50f5089a47c546cf187e9057
Garen Liang <ideabreeze@gmail.com>
2017-02-18 17:38:49 -0800

    Create 03Calculator.js

---
+++ 03Calculator.js
@@ -0,0 +1,26 @@
+/*
+Have the function Calculator(str) take the str parameter being passed and evaluate the mathematical expression within in.
+For example, if str were "2+(3-1)*3" the output should be 8. Another example: if str were "(2-0)(6/2)" the output should be 6.
+There can be parenthesis within the string so you must evaluate it properly according to the rules of arithmetic. 
+The string will contain the operators: +, -, /, *, (, and ). If you have a string like this: #/#*# or #+#(#)/#, 
+then evaluate from left to right. So divide then multiply, and for the second one multiply, divide, then add. 
+The evaluations will be such that there will not be any decimal operations, so you do not need to account for rounding and whatno
+
+*/
+function Calculator(str) { 
+  var output=str[0];
+  for(var i=1;i<str.length;i++){
+    if(str[i]=='('&&(str[i-1]==')'||!isNaN(str[i-1]))){
+      output+='*';
+    }
+    output+=str[i];
+  }
+  return eval(output)
+  // code goes here  
+  return str; 
+}
+   
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+Calculator(readline());
